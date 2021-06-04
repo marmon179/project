f2810872e933167604f8128003f99df88050c49b
muzyk0 <ru9art@gmail.com>
2021-06-04 18:42:28 +0300

    Refactoring

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,10 @@
  * Dev version true/false
  * */
 export const DEV_VERSION = false
+
+export const emailMessage: string = `<div style="background-color: lime; padding: 15px">
+                                        <p>Восстановления пароля.Нажмите на ссылку:</p>
+                                        <a href='https://marmon179.github.io/project/#/set-new-password/$token$'>link</a>
+                                    </div>`
+
+export const sendMessageMethod =  DEV_VERSION ? '' : emailMessage
